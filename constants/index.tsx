8a40eac78ab38ce40ae62db331c0ab6bbc10219f
cjjeon <changjoo.jeon@lucidhr.ca>
2021-09-11 10:25:31 -0500

    Initial Project with Sidebars attached

---
+++ constants/index.tsx
@@ -0,0 +1,48 @@
+import TwitterIcon from "../assets/icons/twitter";
+import InstagramIcon from "../assets/icons/instagram";
+import GithubIcon from "../assets/icons/github";
+import LinkedInIcon from "../assets/icons/linkedIn";
+
+interface INavigation {
+  name: string;
+  link: string;
+}
+
+export const NAVIGATIONS: INavigation[] = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Blog",
+    link: "/blog",
+  },
+  {
+    name: "About Me",
+    link: "/about-me",
+  },
+];
+
+interface ISocialMedia {
+  icon: React.FC;
+  link: string;
+}
+
+export const SOCIALMEDIAS: ISocialMedia[] = [
+  {
+    icon: TwitterIcon,
+    link: "https://twitter.com",
+  },
+  {
+    icon: InstagramIcon,
+    link: "https://instagram.com",
+  },
+  {
+    icon: GithubIcon,
+    link: "https://twitter.com",
+  },
+  {
+    icon: LinkedInIcon,
+    link: "https://twitter.com",
+  },
+];
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../components/layout";
+
+export default function Home() {
+  return (
+    <div className="md:flex flex-col md:flex-row md:min-h-screen w-full">
+      <Navbar />
+      <div>
+        alksndflkansdklfnalksdnflkasndlkfnalksdnflkansdlkfnlksadnflkansdlk
+      </div>
+    </div>
+  );
+}
